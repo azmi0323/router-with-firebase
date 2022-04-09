b5f3ca7b367a796532d2d9df57b2146524ba40f7
AzmiHabib <azmihabib0323@gmail.com>
2022-04-09 19:54:32 +0600

    commit after login and register component

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -3,7 +3,13 @@ import React from 'react';
 const Login = () => {
     return (
         <div>
-            this is login
+            <form className='mt-4'>
+                <input className='px-3 py-2 m-2' type="email" name="" id="" placeholder='Enter Your Email'/>
+                <br />
+                <input className='px-3 py-2 m-2' type="password" placeholder='Your password' name="" id="" />
+                <br />
+                <input className='btn btn-primary' type="button" value="Login" />
+            </form>
         </div>
     );
 };
--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -3,7 +3,15 @@ import React from 'react';
 const Register = () => {
     return (
         <div>
-            
+            <form className='mt-4'>
+                <input className='px-3 py-2 m-2' type="text" placeholder='Please Enter Your Name'/>
+                <br />
+                <input className='px-3 py-2 m-2' type="email" name="" id="" placeholder='Enter Your Email'/>
+                <br />
+                <input className='px-3 py-2 m-2' type="password" placeholder='Your password' name="" id="" />
+                <br />
+                <input className='btn btn-primary' type="button" value="Register" />
+            </form>
         </div>
     );
 };
