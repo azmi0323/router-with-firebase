f0296c8aa4720dbc6f9fefeafdd5a98b1e249549
AzmiHabib <azmihabib0323@gmail.com>
2022-04-09 16:06:48 +0600

    routes created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './Components/Home/Home';
+import Login from './Components/Login/Login';
 
 function App() {
   return (
     <div className="App">
-      <h1>new project</h1>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+      </Routes>
     </div>
   );
 }
