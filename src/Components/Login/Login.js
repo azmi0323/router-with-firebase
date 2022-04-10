434c0c61184696d1e8b74734a64b008cdee9630d
AzmiHabib <azmihabib0323@gmail.com>
2022-04-11 01:41:04 +0600

    commit after location state set

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -2,12 +2,23 @@ import React from "react";
 import { getAuth } from "firebase/auth";
 import app from "../../Firebase.init";
 import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const auth = getAuth(app);
 
 const Login = () => {
   const [signInWithGoogle] = useSignInWithGoogle(auth);
-  
+  const location = useLocation();
+  const navigate = useNavigate();
+  const from = location?.state?.from?.pathname || "/";
+
+  const handleGoogleSignIn = () => {
+    signInWithGoogle()
+    .then(() => {
+      navigate(from, { replace: true });
+    });
+  };
+
   return (
     <div>
       <form className="mt-4">
@@ -30,9 +41,7 @@ const Login = () => {
         <input className="btn btn-primary" type="button" value="Login" />
         <br />
         <input
-          onClick={() => 
-            signInWithGoogle()
-          }
+          onClick={() => handleGoogleSignIn()}
           className="btn mt-2 btn-info"
           type="button"
           value="SignIn With Google"
