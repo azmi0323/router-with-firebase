2fc1a322bcec182ae6b5be1b6845bcbd41d68393
AzmiHabib <azmihabib0323@gmail.com>
2022-04-10 23:48:30 +0600

    cng

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,17 +1,22 @@
 import React from "react";
-import './Header.css'
+import "./Header.css";
 import { Link } from "react-router-dom";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { getAuth } from "firebase/auth";
+import app from "../../Firebase.init";
 
+const auth = getAuth(app);
 const Header = () => {
+  const [user] = useAuthState(auth);
   return (
     <div className="header">
       <nav>
-      <Link to="/">Home</Link>
-      <Link to="/products">Products</Link>
-      <Link to="/orders">Orders</Link>
-      <Link to="/reviews">Reviews</Link>
-      <Link to="/login">Login</Link>
-      <Link to="/register">Register</Link>
+        <Link to="/">Home</Link>
+        <Link to="/products">Products</Link>
+        <Link to="/orders">Orders</Link>
+        <Link to="/reviews">Reviews</Link>
+        <Link to="/login">Login</Link>
+        <Link to="/register">Register</Link>
       </nav>
     </div>
   );
--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,19 +1,41 @@
-import React from 'react';
+import React from "react";
+import { getAuth } from "firebase/auth";
+import app from "../../Firebase.init";
+import {useSignInWithGoogle} from 'react-firebase-hooks/auth'
+
+const auth = getAuth(app);
 
 const Login = () => {
-    return (
-        <div>
-            <form className='mt-4'>
-                <input className='px-3 py-2 m-2' type="email" name="" id="" placeholder='Enter Your Email'/>
-                <br />
-                <input className='px-3 py-2 m-2' type="password" placeholder='Your password' name="" id="" />
-                <br />
-                <input className='btn btn-primary' type="button" value="Login" />
-                <br />
-                <input className='btn mt-2 btn-info' type="button" value="SignIn With Google" />
-            </form>
-        </div>
-    );
+    const [signInWithGoogle,user]=useSignInWithGoogle(auth);
+  return (
+    <div>
+      <form className="mt-4">
+        <input
+          className="px-3 py-2 m-2"
+          type="email"
+          name=""
+          id=""
+          placeholder="Enter Your Email"
+        />
+        <br />
+        <input
+          className="px-3 py-2 m-2"
+          type="password"
+          placeholder="Your password"
+          name=""
+          id=""
+        />
+        <br />
+        <input className="btn btn-primary" type="button" value="Login" />
+        <br />
+        <input onClick={()=>{signInWithGoogle()}}
+          className="btn mt-2 btn-info"
+          type="button"
+          value="SignIn With Google"
+        />
+      </form>
+    </div>
+  );
 };
 
 export default Login;
